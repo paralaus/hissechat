7fcba0768618cb6e9927955760262b469a0b0c4a
paralausalchemy <bulent.cetin@alchemy.com.tr>
2026-01-18 14:23:30 +0300

    Update video.js

--- src/utils/video.js
+++ src/utils/video.js
@@ -1,2 +1,16 @@
+export const VIDEO_LIMITS = {
+    allowedExtensions: ['.mp4', '.mov', '.avi'],
+};
+
+export const validateVideo = (file) => {
+    // a placeholder implementation
+    return true;
+};
+
+export const processVideoForUpload = (file) => {
+    // a placeholder implementation
+    return file;
+};
+
 const video = {};
 export default video;
